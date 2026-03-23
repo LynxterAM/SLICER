52ad1fba52bbb4479a4fb48eb8444627bee28e47
supermerill <merill@free.fr>
2026-03-23 02:37:26 +0100

    move icons name to Gui_app function use bitmapbundle to get the scaled bitmap instead of trying to get the file directly

--- src/slic3r/GUI/GUI_App.hpp
+++ src/slic3r/GUI/GUI_App.hpp
@@ -197,7 +197,9 @@ public:
     bool is_editor() const { return m_app_mode == EAppMode::Editor; }
     bool is_gcode_viewer() const { return m_app_mode == EAppMode::GCodeViewer; }
     bool is_recreating_gui() const { return m_is_recreating_gui; }
-    std::string logo_name() const { return is_editor() ? SLIC3R_APP_KEY : GCODEVIEWER_APP_KEY; }
+    std::string logo_name() const;
+    std::string dark_icon_name() const;
+    std::string light_icon_name() const;
 
     //AngelScript::asIScriptEngine* get_script_engine() const { return m_script_engine.get(); }
 
